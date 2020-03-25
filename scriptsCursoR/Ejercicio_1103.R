d19cb25c3e6ebdff04614c210e85df68f3c3ef76
Javier Porras Gómez <javierporrasgomez@MacBook-Pro-de-Javier.local>
2020-03-25 16:14:16 +0100

    Reorganizado

---
+++ scriptsCursoR/Ejercicio_1103.R
@@ -0,0 +1,16 @@
+#Ejercicio 1.10.3
+#Exercises 1.10 manual R
+log(100)*cos(pi)
+x<-10
+y<-20
+x*y
+z<-x*y
+ls()
+myvec<-c(x,y,z)
+min(myvec)
+max(myvec)
+length(myvec)
+var(myvec)
+#Siguiendo instrucciones del manual como un monito
+#Tengo que escribir varios comentarios 
+#Asegurando que mi script is tottally reproducible
